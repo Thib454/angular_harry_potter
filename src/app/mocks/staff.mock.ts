15cee18f9c2a9c23e36769e2be0d420cf4b3e26e
Thib454 <thibaultdepeige@gmail.com>
2026-01-12 16:03:30 +0100

    Mise en commentaire du mocks

--- src/app/mocks/staff.mock.ts
+++ src/app/mocks/staff.mock.ts
@@ -1,22 +1,22 @@
-import type { Staff } from '../shared/models/staff.model';
+// import type { Staff } from '../shared/models/staff.model';
 
-export const STAFF: Staff[] = [
-  {
-    id: '1',
-    name: 'Minerva McGonagall',
-    image: 'https://hp-api.onrender.com/images/mcgonagall.jpg',
-    house: 'Gryffindor',
-    gender: 'Female',
-    actor: 'Dame Maggie Smith',
-    alive: true,
-    },
-    {
-    id: '2',
-    name: 'Severus Snape',
-    image: 'https://hp-api.onrender.com/images/snape.jpg',
-    house: 'Slytherin',
-    gender: 'Male',
-    actor: 'Alan Rickman',
-    alive   : false,
-    },
-];
+// export const STAFF: Staff[] = [
+//   {
+//     id: '1',
+//     name: 'Minerva McGonagall',
+//     image: 'https://hp-api.onrender.com/images/mcgonagall.jpg',
+//     house: 'Gryffindor',
+//     gender: 'female',
+//     actor: 'Dame Maggie Smith',
+//     alive: true,
+//     },
+//     {
+//     id: '2',
+//     name: 'Severus Snape',
+//     image: 'https://hp-api.onrender.com/images/snape.jpg',
+//     house: 'Slytherin',
+//     gender: 'male',
+//     actor: 'Alan Rickman',
+//     alive   : false,
+//     },
+// ];
