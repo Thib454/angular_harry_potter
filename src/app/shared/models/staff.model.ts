679a3e5024e26550febd07b6a20e38efe879747e
Thib454 <thibaultdepeige@gmail.com>
2026-01-12 09:30:22 +0100

    Ajout du models et service Staff

---
+++ src/app/shared/models/staff.model.ts
@@ -0,0 +1,10 @@
+export interface Staff {
+    id: string;
+    name: string;
+    role?: string;
+    image: string;
+    house: string;
+    gender: string;
+    actor: string;
+    alive: boolean;
+}
---
+++ src/app/shared/services/staff-service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { STAFF } from '../../mocks/staff.mock';
+import { HttpClient } from '@angular/common/http';
+import { Staff } from '../models/staff.model';
+import { of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StaffService {
+  constructor(private http: HttpClient) { }
+
+  getStaff() {
+    // Simulated HTTP request using mock data
+  return this.http.get<Staff[]>(
+    'https://hp-api.onrender.com/api/characters/staff'
+  );
+    // For now, returning mock data as an observable
+    // return of(STAFF);
+  };
+}
